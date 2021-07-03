ecb0bc04d6b2491f07d25947c51fd38740ec93f5
Belov <yevgeniyb@checkpoint.com>
2021-07-03 19:15:46 +0300

    update ignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // logLevel: 'info',
-  // clearScreen: false,
   server: {
-    // hmr: { overlay: false },
+    port: 3000
   },
-  build: {
-    // cssCodeSplit: false,
-  },
-  // server: {
-  //   watch: {
-  //     ignored: './keep/data.json'
-  //   }
-  // },
   plugins: [vue()]
 })
