e80f0815aecc459e2014cabbc6007a5ee2a56a2e
Belov <yevgeniyb@checkpoint.com>
2021-09-19 12:08:54 +0300

    Reorganize css files.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
-import './index.css'
+import './berries-docs.css'
 
 createApp(App).use(router).mount('#app')
